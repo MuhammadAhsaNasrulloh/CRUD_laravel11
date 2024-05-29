1063c97318e815fa75ae1ab586656436b227d113
MuhammadAhsaNasrulloh <san.asrullah07@gmail.com>
2024-05-29 22:10:53 +0700

    First Upload in this Night

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Muhammad Ahsan</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="font-family: 'Poppins', sans-serif">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">Tutorial Laravel 11 untuk Pemula</h3>
+                    <h5 class="text-center">Muhammad Ahsan Nasrulloh</h5>
+                    <hr>
+                </div>
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">ADD PRODUCT</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">IMAGE</th>
+                                    <th scope="col">TITLE</th>
+                                    <th scope="col">PRICE</th>
+                                    <th scope="col">STOCK</th>
+                                    <th scope="col" style="width: 20%">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($products as $barang)
+                                    <tr>
+                                        <td class="text-center">
+                                            <img src="{{ asset('storage/products/'.$barang->image) }}"class= "rounded"style="width: 150px">
+                                        </td>
+                                        <td>{{ $barang->title }}</td>
+                                        <td>{{ "Rp " . number_format($barang->price,2,',','.') }}</td>
+                                        <td>{{ $barang->stock }}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('products.destroy', $barang->id) }}" method="POST">
+                                                <a href="{{ route('products.show', $barang->id) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                                <a href="{{ route('products.edit', $barang->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <div class="alert alert-danger">
+                                        Data Products belum Tersedia.
+                                    </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        //message with sweetalert
+        if(session('success'))
+            Swal.fire({
+                icon: "success",
+                title: "BERHASIL",
+                text: "{{ session('success') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        elseif(session('error'))
+            Swal.fire({
+                icon: "error",
+                title: "GAGAL!",
+                text: "{{ session('error') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        endif
+
+    </script>
+
+</body>
+</html>
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Muhammad Ahsan</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="font-family: 'Poppins', sans-serif">
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <img src="{{ asset('/storage/products/'.$product->image) }}" class="rounded" style="width: 100%">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <h3>{{ $product->title }}</h3>
+                        <hr/>
+                        <p>{{ "Rp " . number_format($product->price,2,',','.') }}</p>
+                        <code>
+                            <p>{!! $product->description !!}</p>
+                        </code>
+                        <hr/>
+                        <p>Stock : {{ $product->stock }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
